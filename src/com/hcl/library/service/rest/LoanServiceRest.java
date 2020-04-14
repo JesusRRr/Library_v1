23de1d60a7703f6488e8ab35c208130150071280
Miguel Partida <miguelpartida080497@gmail.com>
2020-04-14 14:22:57 -0500

    feature: create new Loan (not tested)

---
+++ src/com/hcl/library/service/rest/LoanServiceRest.java
@@ -0,0 +1,27 @@
+package com.hcl.library.service.rest;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import com.hcl.library.exceptions.CustomerHasActiveLoanException;
+import com.hcl.library.service.LoanService;
+import com.hcl.library.service.rest.request.Loan;
+
+@Path("/loans")
+public class LoanServiceRest {
+	LoanService service = LoanService.getLoanService();
+
+	
+	public Response createNewLoan(Loan loanRequest) {
+		int id;
+		try {
+			id = service.createLoan(loanRequest);
+		}catch(CustomerHasActiveLoanException e) {
+			e.printStackTrace();
+			return Response.status(400).entity(e).build();
+		}
+		
+		return Response.status(201).entity(id).build();
+	}
+
+}
