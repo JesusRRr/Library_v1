5779a1cc87cdfdfa11b82ab265b4f048aa65950d
Miguel Partida <miguelpartida080497@gmail.com>
2020-04-22 14:16:49 -0500

    chore: new exceptions for loan

---
+++ src/com/hcl/library/exceptions/LoanException.java
@@ -0,0 +1,15 @@
+package com.hcl.library.exceptions;
+
+public class LoanException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public LoanException() {
+		super();
+	}
+	
+	public LoanException(String message) {
+		super(message);
+	}
+
+}
